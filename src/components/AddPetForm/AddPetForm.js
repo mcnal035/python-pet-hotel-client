3413cfd407dbd14eeb32f367be9421b1ff22e3ce
Molly Ellison <vonnegutter1@gmail.com>
2019-08-21 15:14:13 -0500

    created add pet form with skeleton information

--- src/components/AddPetForm/AddPetForm.js
+++ src/components/AddPetForm/AddPetForm.js
@@ -1,20 +1,66 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 
 
 class addPetForm extends Component  {
 
+  componentDidMount (){
+    // this.props.dispatch({ type: 'GET_PETS', payload: })
+  }
 
+  state = {
+    newPet: {
+      name: '',
+      color: '',
+      breed: '',
+    }
+  }
+
+  handleChange = (propertyName, event) => {
+    console.log('event happened')
+    this.setState({
+      newPet: {
+        ...this.state.newPet,
+        [propertyName]: event.target.value,
+      }
+    });
+  }
 
 
   render() {
   return (
+    <>
     <div className="App">
       <header>
-      <h1>Add Pets Form</h1>
+      <h1>Add Pet Form</h1>
       </header>
     </div>
+
+    <div>
+    <form>
+        <input placeholder="Pet Name" value={this.state.newPet.name}
+            onChange={(event) => this.handleChange('name', event)}></input>
+
+        <input placeholder="Pet Color" value={this.state.newPet.color}
+            onChange={(event) => this.handleChange('color', event)}></input>
+
+        <input placeholder="Pet Breed" value={this.state.newPet.breed}
+            onChange={(event) => this.handleChange('breed', event)}></input>
+          
+        <select>
+          <option placeholder="Owner Name"></option>
+        </select>
+
+        <button type="submit">Submit</button>
+    </form>
+    </div>
+    </>
   );
   }
 }
 
-export default addPetForm;
+const mapStateToProps = reduxStore => ({
+  reduxStore
+})
+
+export default connect(mapStateToProps)(addPetForm);
