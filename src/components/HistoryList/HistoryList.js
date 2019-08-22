c1d274167de122f4f1072645ead9b774afa46397
Sean Olson <srolson@callutheran.edu>
2019-08-22 11:22:13 -0500

    pet history rendering under add pet form on DOM

--- src/components/HistoryList/HistoryList.js
+++ src/components/HistoryList/HistoryList.js
@@ -1,19 +1,21 @@
 import React, {Component} from 'react';
+import '../App/App';
 
 
 class HistoryList extends Component  {
 
-
-
-
   render() {
   return (
-    <div className="App">
-      <header>
-      <h1>Add Pet Form</h1>
-      {/* History list will display the list pf pets */}
-      </header>
-    </div>
+    <>
+      {/* History list will display the list of pets */}
+      <tr>
+      <td>{this.props.item.owner_name}</td>
+      <td>{this.props.item.name}</td>
+      <td>{this.props.item.breed}</td>
+      <td>{this.props.item.color}</td>
+      <td>{this.props.item.checked_in}</td>
+      </tr>
+    </>
   );
   }
 }
