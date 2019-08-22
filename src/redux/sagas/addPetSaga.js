03df32d0f39b0bd9c9a75dac807688cc5548f68b
Mitch Olson <mitchjolson@gmail.com>
2019-08-22 13:58:50 -0500

    fixed URL for addpet and addowner sagas

--- src/redux/sagas/addPetSaga.js
+++ src/redux/sagas/addPetSaga.js
@@ -5,7 +5,8 @@ import axios from 'axios';
 function* addPet(action) {
     console.log('in addPet, action.payload is', action.payload);
     try {
-        yield axios.post(`/addpets`, action.payload);
+        yield axios.post(`/addpets?name=${action.payload.name}&breed=${action.payload.breed}&color=${action.payload.color}&owner_name=${action.payload.owner_name}&checked_in=no`);
+        // yield axios.post(`/addpets`, action.payload);
         yield put({ type: 'FETCH_PETS' });
     }
     catch (error) {
